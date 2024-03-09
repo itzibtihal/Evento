ada266c6e49a57a9e4410260a6b87e3122337bcc
itzibtihal <ibtihalboukhanchouch@gmail.com>
2024-03-09 22:22:16 +0100

    add price filter

--- resources/views/Guest/events/index.blade.php
+++ resources/views/Guest/events/index.blade.php
@@ -18,43 +18,45 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.css') }}">
-    
+
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/owl-carousel.css') }}">
-    
+
     <link rel="stylesheet" href="{{ asset('assets/css/tooplate-artxibition.css') }}">
-    
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
     <style>
         .pagination {
-    margin-top: 20px;
-    text-align: center;
-}
+            margin-top: 20px;
+            text-align: center;
+        }
 
-.pagination ul {
-    display: inline-block;
-    padding: 0;
-    margin: 0;
-}
+        .pagination ul {
+            display: inline-block;
+            padding: 0;
+            margin: 0;
+        }
 
-.pagination li {
-    display: inline;
-    margin: 0 2px;
-    text-align: center;
-}
+        .pagination li {
+            display: inline;
+            margin: 0 2px;
+            text-align: center;
+        }
 
-.pagination li a, .pagination .disabled {
-    display: inline-block;
-    padding: 8px 16px;
-    text-decoration: none;
-    color: #333;
-    background-color: #f8f9fa;
-    border: 1px solid #dee2e6;
-    border-radius: 4px;
-}
+        .pagination li a,
+        .pagination .disabled {
+            display: inline-block;
+            padding: 8px 16px;
+            text-decoration: none;
+            color: #333;
+            background-color: #f8f9fa;
+            border: 1px solid #dee2e6;
+            border-radius: 4px;
+        }
 
-.pagination li.active a {
-    background-color: #007bff;
-    color: #fff;
-}
+        .pagination li.active a {
+            background-color: #007bff;
+            color: #fff;
+        }
     </style>
 
 </head>
@@ -88,7 +90,7 @@
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
-                            <li><a href="{{ route('guest.index') }}" >Home</a></li>
+                            <li><a href="{{ route('guest.index') }}">Home</a></li>
                             <li><a href="{{ route('guest.event.index') }}" class="active">Shows & Events</a></li>
                             <li><a href="{{ route('mytickets') }}"> My Tickets</a></li>
                             <li><a href="{{ route('logout') }}"
@@ -155,16 +157,18 @@
                                             </select>
                                         </div>
                                         <div class="col-lg-3">
-                                            <select value="month">
-                                                <option value="month">Price</option>
-                                                <option name="min" id="min">$0 - $50</option>
-                                                <option name="standard" id="standard">$50 - $100</option>
-                                                <option name="hight" id="hight">$100 - $200</option>
+                                            
+                                            <select id="priceFilter" name="priceFilter">
+                                                <option value="all">All</option>
+                                                <option value="0-50">$0 - $50</option>
+                                                <option value="50-100">$50 - $100</option>
+                                                <option value="up-to-100">$100~</option>
                                             </select>
                                         </div>
                                         <div class="col-lg-3">
                                             <fieldset>
-                                            <button type="submit" id="form-submit" class="main-dark-button">Submit</button>
+                                                <button type="submit" id="form-submit"
+                                                    class="main-dark-button">Submit</button>
                                             </fieldset>
                                         </div>
                                     </div>
@@ -179,51 +183,96 @@
                     </div>
                 </div>
                 @foreach ($events as $event)
-                <div class="col-lg-4">
-                    <div class="ticket-item">
-                        <div class="thumb">
-                            @if($event->getFirstMediaUrl('media/events'))
-                                <img src="{{$event->getFirstMediaUrl('media/events')}}" alt="Event Image">
-                               
+                    <div class="col-lg-4 ticket-filtre"  data-price="{{ $event->ticket_price }}">
+                        <div class="ticket-item">
+                            <div class="thumb">
+                                @if ($event->getFirstMediaUrl('media/events'))
+                                    <img src="{{ $event->getFirstMediaUrl('media/events') }}" alt="Event Image">
                                 @else
                                     <img src="{{ asset('assets/images/ticket-03.jpg') }}" alt="Default Image">
                                 @endif
-                            <div class="price">
-                                <span>1 ticket<br>from <em>${{ $event->ticket_price }}</em></span>
+                                <div class="price">
+                                    <span>1 ticket<br>from <em>${{ $event->ticket_price }}</em></span>
+                                </div>
                             </div>
-                        </div>
-                        <div class="down-content">
-                            <span>There Are {{ $event->available_tickets }} Tickets Left For This Show</span>
-                            <h4>{{ $event->title }}</h4>
-                            <ul>
-                                <li><i class="fa fa-clock-o"></i> {{ $event->date }} at {{ $event->hour }}</li>
-                                <li><i class="fa fa-map-marker"></i>{{ $event->lieu }}</li>
-                            </ul>
-                            <div class="main-dark-button">
-                                <a href="{{ route('event.details', ['event' => $event->id]) }}">Purchase Tickets</a>
+                            <div class="down-content">
+                                <span>There Are {{ $event->available_tickets }} Tickets Left For This Show</span>
+                                <h4>{{ $event->title }}</h4>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> {{ $event->date }} at {{ $event->hour }}</li>
+                                    <li><i class="fa fa-map-marker"></i>{{ $event->lieu }}</li>
+                                </ul>
+                                <div class="main-dark-button">
+                                    <a href="{{ route('event.details', ['event' => $event->id]) }}">Purchase
+                                        Tickets</a>
+                                </div>
                             </div>
                         </div>
                     </div>
+                @endforeach
+            </div>
+
+            <div class="col-lg-12">
+                <div class="pagination">
+                    {{ $events->links() }}
                 </div>
-            @endforeach
-        </div>
-    
-        <div class="col-lg-12">
-            <div class="pagination">
-                {{ $events->links() }}
             </div>
-        </div>
-    
-</div>
-            </div>
+
         </div>
     </div>
+    </div>
+    </div>
+    <script>$(document).ready(function () {
+        // Add an event listener to the price filter dropdown
+        $('#priceFilter').change(function () {
+            // Get the selected value
+            var selectedPriceFilter = $(this).val();
+    
+            // Filter events based on the selected price range
+            filterEvents(selectedPriceFilter);
+        });
+    
+        // Function to filter events
+        function filterEvents(priceFilter) {
+            // Get all event items
+            var events = $('.ticket-filtre');
+    
+            // Show all events initially
+            events.show();
+    
+            // Check if a price filter is selected
+            if (priceFilter !== 'all') {
+                // Hide events that don't match the selected price range
+                events.filter(function () {
+                    var eventPrice = parseFloat($(this).data('price'));
+                    return !isPriceInRange(eventPrice, priceFilter);
+                }).hide();
+            }
+        }
+    
+        // Function to check if a price is in the selected range
+        function isPriceInRange(price, range) {
+            // Handle the "max up to 100" condition
+            if (range === 'up-to-100') {
+                return price > 100;
+            }
+    
+            // For other ranges, parse and compare as before
+            var rangeValues = range.split('-');
+            var minPrice = parseFloat(rangeValues[0]);
+            var maxPrice = parseFloat(rangeValues[1]);
+    
+            return price >= minPrice && price <= maxPrice;
+        }
+    });
+    
+    </script>
+    
 
 
 
 
 
-   
 
 
 
@@ -341,7 +390,7 @@
 
     <script src="{{ asset('assets/js/popper.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    
+
     <script src="{{ asset('assets/js/scrollreveal.min.js') }}"></script>
     <script src="{{ asset('assets/js/waypoints.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
@@ -349,7 +398,7 @@
     <script src="{{ asset('assets/js/mixitup.js') }}"></script>
     <script src="{{ asset('assets/js/accordions.js') }}"></script>
     <script src="{{ asset('assets/js/owl-carousel.js') }}"></script>
-    
+
     <script src="{{ asset('assets/js/custom.js') }}"></script>
 </body>
 
